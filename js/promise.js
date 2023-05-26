ee5b65b888de691a274166956f902cbb06f263e0
yeakode <hiwrld1010@gmail.com>
2023-05-26 23:02:00 +0900

    promise

---
+++ js/promise.js
@@ -0,0 +1,49 @@
+function taskA(a, b) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const res = a + b;
+      cb(res);
+    }, 3000);
+  });
+}
+function taskB(a) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const res = a * 2;
+      cb(res);
+    }, 1000);
+  });
+}
+function taskC(a) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const res = a * -1;
+      cb(res);
+    }, 2000);
+  });
+}
+
+taskA(5, 1)
+  .then((a_res) => {
+    console.log("A의 결과: ", a_res);
+    return taskB(a_res);
+  })
+  .then((b_res) => {
+    console.log("B의 결과: ", b_res);
+    return taskC(b_res);
+  })
+  .then((c_res) => {
+    console.log("C의 결과: ", c_res);
+  });
+
+/******************안 좋은 예시*******************/
+
+taskA(3, 4, (a_res) => {
+  console.log("A의 결과: ", a_res);
+  taskB(a_res, (b_res) => {
+    console.log("B의 결과: ", b_res);
+    taskC(b_res, (c_res) => {
+      console.log("C의 결과: ", c_res);
+    });
+  });
+});
