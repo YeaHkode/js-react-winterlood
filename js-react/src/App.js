d37b8c2d10dc7edd49ada4e6c774c8bf6d67841e
yeakode <hiwrld1010@gmail.com>
2023-05-29 17:17:10 +0900

    인라인 스타일링

--- js-react/src/App.js
+++ js-react/src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import MyFooter from "./MyFooter";
 import MyHeader from "./MyHeader";
 
 function App() {
   let name = "kang";
 
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    b: {
+      color: "green",
+    },
+  };
+
   return (
-    <div className="App">
+    <div style={style.App}>
       <MyHeader />
 
-      <h2>안녕 리액트 {name}</h2>
-      <b id="bold_text">React.js</b>
+      <h2 style={style.h2}>안녕 리액트 {name}</h2>
+      <b style={style.b}>React.js</b>
       <MyFooter />
     </div>
   );
