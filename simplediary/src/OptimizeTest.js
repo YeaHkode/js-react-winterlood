846851b8df34f97ebed97be3ed7087d7506a1b41
yeakode <hiwrld1010@gmail.com>
2023-06-03 19:11:41 +0900

    feat: areEqual 함수

--- simplediary/src/OptimizeTest.js
+++ simplediary/src/OptimizeTest.js
@@ -1,33 +1,55 @@
 import React, { useState, useEffect } from "react";
 
-const TextView = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`${text} 업데이트 됨`);
-  });
-  return <div>{text}</div>;
-});
-const CountView = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`${count} 업데이트 됨`);
+    console.log(`CounterA Updated: ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = ({ obj }) => {
+  useEffect(() => {
+    console.log(`counterB Updated: ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  if (prevProps.obj.count === nextProps.obj.count) {
+    return true;
+  }
+  return false;
+  // return prevProps.obj.count === nextProps.obj.count
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A 버튼</button>
       </div>
       <div>
-        <h2>text</h2>
-        <TextView text={text} />
-        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+
+        <button
+          onClick={() =>
+            setObj({
+              count: obj.count,
+            })
+          }
+        >
+          B 버튼
+        </button>
       </div>
     </div>
   );
