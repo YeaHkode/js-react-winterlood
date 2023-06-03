e6e98a16458adcd144d0e0cd28d92ef808d49b30
yeakode <hiwrld1010@gmail.com>
2023-06-03 15:39:13 +0900

    lifecycle

--- simplediary/src/Lifecycle.js
+++ simplediary/src/Lifecycle.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+// 하나의 파일에 두 개의 컴포넌트도 가능은 함. 그러나 가독성 때문에 추천은 안 함.
 const UnmountTest = () => {
   useEffect(() => {
     console.log("Mount!");
